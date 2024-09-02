714b2a62a8d0da8f7ef6e2ba2fdf7b31feeb3d53
Thomasvhj <thomasv1702@gmail.com>
2024-09-02 15:02:38 +0200

    Camera fix
    Nogenlunde fixed camera movements og collision boxes

--- freestyle/Scripts/Level1.cs
+++ freestyle/Scripts/Level1.cs
@@ -13,13 +13,17 @@ public partial class Level1 : Node2D
 	{
 		Player1 = GetNode<CharacterBody2D>("Player");
 		Player2 = GetNode<CharacterBody2D>("Player2");
-		Camera = GetNode<Camera2D>("Camera2D");
+		Camera = GetNode<Camera2D>("Camera");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		float CameraX = (Player1.Position.X + Player2.Position.X)/2;
+		if(CameraX < 0){
+			CameraX = 0;
+		}
+		GD.Print("CamPos: "+ CameraX);
 		Camera.Position = new Vector2(CameraX,0);
 			if (Input.IsActionJustPressed("switch"))
 			{

c7f47cd29e006541bc4b57706f7316a998b4216e
Thomasvhj <thomasv1702@gmail.com>
2024-09-09 13:28:23 +0200

    Bedre sceneskift i starten

---
+++ freestyle/Scripts/Start.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Start : Node2D
+{
+	public override void _Ready()
+	{
+	//GetTree().Root.AddChild(ResourceLoader.Load<PackedScene>("res://Scenes/Level0.tscn").Instantiate());
+	//GetTree().Root.RemoveChild(ResourceLoader.Load<PacketScene>("res://Scenes/world.tscn").Instantiate());
+	}
+}
